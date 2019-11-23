ab8ab429d893d750caadde17c34e54073bf246f0
jamshid <jamshidazizov@gmail.com>
2019-11-24 00:09:02 +0200

    sign-in form updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
 
 
@@ -20,6 +21,7 @@ function App() {
 	    <Switch>
 	    <Route exact={true} path='/' component={HomePage} />
 	    <Route exact={true} path='/shop' component={ShopPage} />
+	    <Route exact={true} path='/signin' component={SignInSignUpPage} />
 	    </Switch>
     </div>
   );
