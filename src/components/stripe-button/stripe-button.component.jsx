62a9ae69cf9f0915cf4cf133afdc1916c9d91fee
jamshid <jamshidazizov@gmail.com>
2019-11-28 12:55:42 +0200

    stripe integration, favicon and title updates

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price*100;
+	const publishableKey = 'pk_test_HcmugIPIUtSRflMk9XKKDigr00EJbvcXsd';
+
+	const onToken = token => {
+		console.log(token);
+		alert('Payment Successful');
+	}
+
+	return (
+		<StripeCheckout 
+			label='Pay Now'
+			name='Zen Inc'
+			billingAddress
+			shippingAddress
+			image='https://svgshare.com/i/GSb.svg'
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel='Pay Now'
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
