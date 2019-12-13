803d42cef4247c6df02c927dbddac30e1f688814
jamshid <jamshidazizov@gmail.com>
2019-12-13 13:47:30 +0200

    move data to firebase

--- src/App.js
+++ src/App.js
@@ -12,16 +12,17 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, addCollectionAndDocuments } = this.props;
+    const { setCurrentUser, collectionsArray, addCollectionAndDocuments } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -38,7 +39,7 @@ class App extends React.Component {
       setCurrentUser(userAuth);
       addCollectionAndDocuments(
         'collections', 
-        collectionsArray.map(({title, items}) => ({ title, items })))
+        collectionsArray.map(({ title, items }) => ({ title, items })))
     });
   }
 
